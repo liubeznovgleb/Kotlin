374600f5da3d694d0383b6c35b02029b36ed8a51
liubeznovgleb <113688749+liubeznovgleb@users.noreply.github.com>
2024-03-28 23:28:29 +0300

    Create Homework.kt

---
+++ Homework.kt
@@ -0,0 +1,46 @@
+import java.io.File
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+
+const val filePath = "C://Kotlin_1.txt"
+const val outputFileName = "Kotlin_1.txt"
+
+
+class City(val name: String, val measurement: Double)
+
+
+fun writeFile(cityToMeasures: Map<String, List<Double>>) {
+    try {
+        val writer = File(outputFileName).bufferedWriter()
+        for ((key, cityList) in cityToMeasures) {
+            val average = BigDecimal(cityList.average()).setScale(1, RoundingMode.HALF_EVEN)
+            val line = "$key=${cityList.minOrNull()}/${average}/${cityList.maxOrNull()}\n"
+            writer.write(line)
+        }
+        writer.close()
+    } catch (ex: Exception) {
+        println("Ошибка записи: ${ex.message}")
+    }
+}
+
+
+fun readFile(cityList: MutableList<City>) {
+    val file = File(filePath)
+    try {
+        file.forEachLine { line ->
+            val (name, measurement) = line.split(";")
+            val city = City(name, measurement.toDouble())
+            cityList.add(city)
+        }
+    } catch (ex: Exception) {
+        println("Ошибка чтения: ${ex.message}")
+    }
+}
+
+fun main() {
+    val cityList: MutableList<City> = mutableListOf()
+    readFile(cityList)
+    val cityToMeasures: Map<String, List<Double>> = cityList.groupBy({ it.name }) { it.measurement }
+    writeFile(cityToMeasures)
+}
